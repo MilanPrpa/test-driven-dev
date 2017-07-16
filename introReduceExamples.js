51c128f309d12e86b181537bf663688d66961e30
MilanPrpa <milanprpa25@gmail.com>
2017-07-15 18:22:20 -0700

    added more files

---
+++ introReduceExamples.js
@@ -0,0 +1,46 @@
+
+
+/*Reduce method reduces the element in the array to one value*/
+
+/*INTRO TO REDUCE*/
+/*Reduce method reduces the element in the array to one value*/
+
+// var numbers = [1, 2, 3, 4];
+
+
+// var sumSoFar = 0;
+// sumSoFar = sumSoFar + 1;
+// sumSoFar = sumSoFar + 2;
+// sumSoFar = sumSoFar + 3;
+// sumSoFar = sumSoFar + 4;
+
+
+//use this adder() function to run sum so far. It is the same as work above.
+// function adder(sumSoFar, nextNumber) {
+// 	return sumSoFar + nextNumber;
+// };
+
+// var sumSoFar = 0;
+
+// sumSoFar = adder(sumSoFar, numbers[0]);
+// sumSoFar = adder(sumSoFar, numbers[1]);
+// sumSoFar = adder(sumSoFar, numbers[2]);
+// sumSoFar = adder(sumSoFar, numbers[3]);
+
+
+/*Final Function*/
+// function reduce(arr, callback, startingValue) {
+// 	var sumSoFar = startingValue;
+// 	for (var i = 0; i < arr.length; i++) {
+// 		sumSoFar = callback(sumSoFar, arr[i]);
+// 	}
+// 	return sumSoFar;
+// }
+
+
+// function sum() {
+// 		for (var i = 0; i < numbers.lenth; i++) {
+// 		 sumSoFar = adder(sumSoFar, numbers[i]);
+// 		 return sumSoFar;
+// }
+// }
